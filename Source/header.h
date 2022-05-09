71ac613c5d4c65065f2a75fedf29624ae6f20fdf
Ngô Văn Trung <86840886+TrungNgoVan@users.noreply.github.com>
2022-05-10 06:19:42 +0700

    main

---
+++ Source/header.h
@@ -0,0 +1,29 @@
+#pragma once
+
+class User
+{
+};
+
+class Student : public User
+{
+};
+
+class Lecturer : public User
+{
+};
+
+class AcademicStaff : public User
+{
+};
+
+class Course
+{
+};
+
+class Attendance
+{
+};
+
+class Score
+{
+};

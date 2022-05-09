0f6251e4a05cb6f68300d53dfd323c7429238f55
Ngô Văn Trung <86840886+TrungNgoVan@users.noreply.github.com>
2022-05-09 23:41:22 +0700

    Day1

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,9 +1,67 @@
-#include<iostream>
-#include"random.h"
+#include<bits/stdc++.h>
+
+
+
+
+class Attendance{
+private:
+    std::string codeCourse;
+    int year;
+public:
+//Constructer
+    Attendance(Course* B){
+        this->codeCourse = B->getCodeCourse();
+    }
+    std::string getCodeCourse(){
+        return this->codeCourse;
+    }
+};
+
+class Course{
+private:
+    std::string codeCourse;
+    std::vector<Student*> listStudent;
+public:
+    void setCodeCourse(std::string value){
+        this->codeCourse = value;
+    }
+    std::string getCodeCourse(){
+        return this->codeCourse;
+    }
+    void addStudent(Student* term){
+        //Do nothin
+    }
+};
+
+class Person{
+
+};
+
+class Student : public Person{
+private:
+    std::vector<Attendance*> listCourse;
+public:
+    void addCourse(Course* B){
+        Attendance* A = new Attendance(B);
+        this->listCourse.push_back(A);
+        B->addStudent(this);
+    }
+    void getCourse(){
+        for (int i = 0 ; i < this->listCourse.size();i++){
+            std::cout << listCourse[i]->getCodeCourse() << std::endl;
+        }
+    }
+};
 
 int main(){
-    Person A;
-    A.setAge(9);
-    std::cout << A.getAge();
+    Student Trung;
+    Course TTH1;
+    TTH1.setCodeCourse("CLC");
+    Trung.addCourse(&TTH1);
     return 0;
 }
+/**
+ * @brief 1 - n 
+ * 1 
+ * 
+ */
--- Source/random.cpp
+++
@@ -1,9 +0,0 @@
-#include"random.h"
-
-int Person::getAge(){
-    return this->_age;
-}
-
-void Person::setAge(int value){
-    this->_age = value;
-}

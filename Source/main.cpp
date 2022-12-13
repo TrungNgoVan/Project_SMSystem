da0ed068f4144f9074cf9153ee100ddac74d7fa8
Ngô Văn Trung <Trung2601.it@gmail.com>
2022-12-13 15:59:51 +0700

    main
    Update

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,56 +1,112 @@
-#include<bits/stdc++.h>
+// #include<bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <ctime>
 
-// class Attendance{
+using namespace std;
+
+class Init{
+public:
+    static void start(){
+        /*
+        struct tm {
+            int tm_sec;   // seconds of minutes from 0 to 61
+            int tm_min;   // minutes of hour from 0 to 59
+            int tm_hour;  // hours of day from 0 to 24
+            int tm_mday;  // day of month from 1 to 31
+            int tm_mon;   // month of year from 0 to 11
+            int tm_year;  // year since 1900
+            int tm_wday;  // days since sunday
+            int tm_yday;  // days since January 1st
+            int tm_isdst; // hours of daylight savings time
+        }
+        */
+        time_t now = time(0);
+        tm *Time  = localtime(&now);
+        cout << Time->tm_hour;
+    }
+};
+
+// class Date
+// {
 // private:
-//     std::string codeCourse;
-//     int year;
+//     int _day;
+//     int _month;
+//     int _year;
 // public:
-// //Constructer
-//     Attendance(Course* B){
-//         this->codeCourse = B->getCodeCourse();
-//     }
-//     std::string getCodeCourse(){
-//         return this->codeCourse;
-//     }
 // };
 
-// class Course{
+// class Time
+// {
 // private:
-//     std::string codeCourse;
-//     std::vector<Student*> listStudent;
+//     int _minute;
+//     int _hour;
 // public:
-//     void setCodeCourse(std::string value){
-//         this->codeCourse = value;
-//     }
-//     std::string getCodeCourse(){
-//         return this->codeCourse;
-//     }
-//     void addStudent(Student* term){
-//         //Do nothin
-//     }
 // };
 
-// class Person{
+class Course
+{
+private:
+    string _courseCode;
+    string _courseName;
+    int _year;
+    int _semester;
+    tm *_start;
+    tm *_end;
+    tm *_from;
+    tm *_to;
+    string _dayOfWeek;
+public:
+};
 
-// };
+class Score
+{
+private:
+    string _courseCode;
+    int _year;
+    int _semester;
+    string _studentID;
+    float _midScore;
+    float _labScore;
+    float _finalScore;
+};
 
-// class Student : public Person{
-// private:
-//     std::vector<Attendance*> listCourse;
-// public:
-//     void addCourse(Course* B){
-//         Attendance* A = new Attendance(B);
-//         this->listCourse.push_back(A);
-//         B->addStudent(this);
-//     }
-//     void getCourse(){
-//         for (int i = 0 ; i < this->listCourse.size();i++){
-//             std::cout << listCourse[i]->getCodeCourse() << std::endl;
-//         }
-//     }
-// };
+class Person
+{
+private:
+    string _userName;
+    string _fullName;
+    string _email;
+    string _mobilePhone;
+    int _type;
+    string _password;
+    string _class;
+};
 
-int main(){
-    
+class Student : Person
+{
+private:
+    string _studentID;
+
+public:
+};
+
+class Lecturer : Person{
+private:
+    string _shortName;
+public:
+
+};
+
+class AcademicStaff : Person{
+private:
+    string _shortName;
+public:
+};
+
+int main()
+{
+
     return 0;
 }
